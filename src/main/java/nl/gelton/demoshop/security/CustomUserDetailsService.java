207d5f2aefb6dcae2e046883b20e814c4c62b656
Ollie Gelton <olliegelton@gmail.com>
2024-10-16 15:59:41 +0200

    AuthUser & CustomUserDetailsService toegevoegd

---
+++ src/main/java/nl/gelton/demoshop/security/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package nl.gelton.demoshop.security;
+
+import lombok.RequiredArgsConstructor;
+import nl.gelton.demoshop.exception.NotFoundException;
+import nl.gelton.demoshop.model.User;
+import nl.gelton.demoshop.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(()-> new NotFoundException("User/ Email Not found"));
+
+        return AuthUser.builder()
+                .user(user)
+                .build();
+    }
+}
