9d996b0c40b7877eea7fbdf3cb99aaf75ee0bded
Ollie Gelton <olliegelton@gmail.com>
2024-10-16 13:41:28 +0200

    User, UserRole en OrderStatus toegevoegd

---
+++ src/main/java/nl/gelton/demoshop/enums/OrderStatus.java
@@ -0,0 +1,11 @@
+package nl.gelton.demoshop.enums;
+
+public enum OrderStatus {
+
+    PENDING,
+    CONFIRMED,
+    SHIPPED,
+    DELIVERED,
+    CANCELLED,
+    RETURNED
+}
---
+++ src/main/java/nl/gelton/demoshop/model/User.java
@@ -0,0 +1,48 @@
+package nl.gelton.demoshop.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import nl.gelton.demoshop.enums.UserRole;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "users")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @Column(unique = true)
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    @NotBlank(message = "Phone number is required")
+    private String phoneNumber;
+
+    private UserRole role;
+
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<OrderItem> orderItemList;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Address address;
+
+    private final LocalDateTime createdAt = LocalDateTime.now();
+}
