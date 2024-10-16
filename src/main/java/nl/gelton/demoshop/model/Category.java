631fdb14f09787705858a03ecedaf075c789232d
Ollie Gelton <olliegelton@gmail.com>
2024-10-16 13:54:21 +0200

    Address, Category en Product models toegevoegd

---
+++ src/main/java/nl/gelton/demoshop/model/Category.java
@@ -0,0 +1,24 @@
+package nl.gelton.demoshop.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    private List<Product> productList;
+
+    private final LocalDateTime createdAt = LocalDateTime.now();
+}
